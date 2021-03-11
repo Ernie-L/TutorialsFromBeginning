efa3f13282a28fab39fa29290152ae4fa92fe6ed
Ernie <erniel@outlook.com>
2021-03-10 22:09:26 -0600

    Move Square into its own file.

--- Explorer/Map.cs
+++ Explorer/Map.cs
@@ -4,13 +4,6 @@ using System.Text;
 
 namespace Explorer
 {
-    //// Goal: 1. Abstract class of Square with different concrete 
-    ///           classes like land and water.
-    ///        2. Have the map store arrays of squares.
-    ///        3. Have the player move around the new map.
-    ///        
-    //// Quiz: Add a new type of square... without changing code outside the Map.cs file.
-
     public class Map
     {
         public Square[,] Squares { get; }
@@ -59,26 +52,5 @@ namespace Explorer
             char display = Squares[x, y].Display;
             Screen.WriteAt(display, x, y);
         }
-    }
-
-    public abstract class Square
-    {
-        public virtual char Display { get; }
-        public virtual string Name { get; }
-        public virtual bool Passable { get; }
-    }
-
-    public class LandSquare : Square
-    {
-        public override char Display { get { return '.'; } }
-        public override string Name { get { return "Land"; } }
-        public override bool Passable { get { return true; } }
-    }
-
-    public class WaterSquare : Square
-    {
-        public override char Display { get { return '~'; } }
-        public override string Name { get { return "Water"; } }
-        public override bool Passable { get { return false; } }
     }
 }
---
+++ Explorer/Square.cs
@@ -0,0 +1,23 @@
+﻿namespace Explorer
+{
+    public abstract class Square
+    {
+        public virtual char Display { get; }
+        public virtual string Name { get; }
+        public virtual bool Passable { get; }
+    }
+
+    public class LandSquare : Square
+    {
+        public override char Display { get { return '.'; } }
+        public override string Name { get { return "Land"; } }
+        public override bool Passable { get { return true; } }
+    }
+
+    public class WaterSquare : Square
+    {
+        public override char Display { get { return '~'; } }
+        public override string Name { get { return "Water"; } }
+        public override bool Passable { get { return false; } }
+    }
+}
