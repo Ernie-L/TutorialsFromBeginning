84f2efad3fd6409624f0137ffdb9e197bf9aa0ea
Ernie <erniel@outlook.com>
2022-01-08 14:35:23 -0600

    Create the beginning of the Robot project.

---
+++ Robot/Tiles.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    internal class Tiles
+    {
+        public static class Robot
+        {
+            public static string Up = @" ______ " + "\n" +
+                                      @"/ v  v \" + "\n" +
+                                      @"|------|" + "\n" +
+                                      @"|      |" + "\n" +
+                                      @"\______/";
+
+            public static string Right = @" ______ " + "\n" +
+                                         @"/  |   \" + "\n" +
+                                         @"|  |  <|" + "\n" +
+                                         @"|  |  <|" + "\n" +
+                                         @"\__|___/";
+
+            public static string Left = @" ______ " + "\n" +
+                                        @"/   |  \" + "\n" +
+                                        @"|>  |  |" + "\n" +
+                                        @"|>  |  |" + "\n" +
+                                        @"\___|__/";
+
+            public static string Down = @" ______ " + "\n" +
+                                        @"/      \" + "\n" +
+                                        @"|      |" + "\n" +
+                                        @"|------|" + "\n" +
+                                        @"\_^__^_/";
+        }
+
+        public static string Grass = @"/\|\/|/|" + "\n" +
+                                     @"\|/\/|/\" + "\n" +
+                                     @"|/\|/|\/" + "\n" +
+                                     @"\/|//\|\" + "\n" +
+                                     @"\\/|\//\";
+
+        public static string Water = @"~/~~/~~~" + "\n" +
+                                     @"~~/~~~~~" + "\n" +
+                                     @"~/~~~~~/" + "\n" +
+                                     @"/~~/~~/~" + "\n" +
+                                     @"~/~~/~~~";
+
+        public static class Door
+        {
+            public static string Closed = @"        " + "\n" +
+                                          @"  ____  " + "\n" +
+                                          @" / || \ " + "\n" +
+                                          @"| O||O |" + "\n" +
+                                          @" \_||_/ ";
+
+            public static string Partial = @"        " + "\n" +
+                                           @"  ____  " + "\n" +
+                                           @" /|  |\ " + "\n" +
+                                           @"|O|  |O|" + "\n" +
+                                           @" \|__|/ ";
+
+            public static string Open = @"        " + "\n" +
+                                        @"  ____  " + "\n" +
+                                        @" /    \ " + "\n" +
+                                        @"||    ||" + "\n" +
+                                        @" \____/ ";
+        }
+    }
+}
