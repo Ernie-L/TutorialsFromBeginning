f15152dab5a94a06144d521e960b791ad87f89ce
Tristan <FlareCool@outlook.com>
2022-01-17 11:19:39 -0600

    all updated from the merge

---
+++ Robot/TStuf/BobTileset.cs
@@ -0,0 +1,38 @@
+﻿namespace Robot
+{
+    public static class BobTileset
+    {
+            public static Tile Up { get; } =
+                new Tile(
+                    new string[Config.Tile.Height] { @"        ",
+                                                    @"  ( )   ",
+                                                    @" /   \  ",
+                                                    @"(/   \) ",
+                                                    @"(_/ \_) " });
+
+            public static Tile Right { get; } =
+                new Tile(
+                    new string[Config.Tile.Height] { @"        ",
+                                                    @"   ( )  ",
+                                                    @"   / |  ",
+                                                    @"  (| |  ",
+                                                    @"   [__) " });
+
+            public static Tile Left { get; } =
+                new Tile(
+                    new string[Config.Tile.Height] { @"        ",
+                                                    @"   ( )  ",
+                                                    @"   | \  ",
+                                                    @"   | |) ",
+                                                    @"  (__]  " });
+
+            public static Tile Down { get; } =
+                new Tile(
+                    new string[Config.Tile.Height] { @"        ",
+                                                    @"  (^^)  ",
+                                                    @" /   \  ",
+                                                    @"(/   \) ",
+                                                    @"(_/ \_) " });
+        
+    }
+}
---
+++ Robot/TStuf/StarterMap.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Robot
+{
+    public class StarterMap : Map
+    {
+        public StarterMap() : base()
+        {
+            Squares = new Square[Width, Height];
+
+            // cover all land
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    Squares[x, y] = new Woodfloor(x, y);
+                }
+            }
+            for (int x = 11; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    Squares[x, y] = new GrassSquare(x, y);
+                }
+            }
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 6; y < Height; y++)
+                {
+                    Squares[x, y] = new GrassSquare(x, y);
+                }
+            }
+            //// add some sea
+            for (int x = 0; x < Width - 5; x++)
+            {
+                for (int y = 2; y < Height - 3; y++)
+                {
+                    Squares[x, y] = new WaterSquare(x, y);
+                }
+            }
+            Squares[0, 4] = new DoorSquare(0, 4, "LEAVE_STARTER_MAP");
+
+            LinkSquares();
+        }
+    }
+}
